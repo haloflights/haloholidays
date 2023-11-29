8f29a3b86145e8fd839eca4c8ca2d1cc22b84d3f
Saad23-Halo <saad@haloflights.co.uk>
2023-11-29 16:25:32 +0530

    404

--- app-directory-project(New)/app/(others)/404/page.jsx
+++ app-directory-project(New)/app/(others)/404/page.jsx
@@ -1,6 +1,6 @@
 import dynamic from "next/dynamic";
 import CallToActions from "@/components/common/CallToActions";
-import DefaultHeader from "@/components/header/default-header";
+import Header3 from "@/components/header/header-3";
 import DefaultFooter from "@/components/footer/default";
 import NotFound from "@/components/common/NotFound";
 
@@ -17,7 +17,7 @@ const index = () => {
       <div className="header-margin"></div>
       {/* header top margin */}
 
-      <DefaultHeader />
+      <Header3 />
       {/* End Header 1 */}
 
       <NotFound />
